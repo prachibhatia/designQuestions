dd28310734e6a91545989404a68fad1111e39653
Prachi Bhatia <prachi24bhatia@gmail.com>
2023-02-21 16:16:32 +0530

    strategy

--- src/main/java/org/example/ticTacToe/strategy/GameWinningStrategy/ColumnWinningStrategy.java
+++ src/main/java/org/example/ticTacToe/strategy/GameWinningStrategy/ColumnWinningStrategy.java
@@ -4,9 +4,30 @@ import org.example.ticTacToe.model.Board;
 import org.example.ticTacToe.model.Cell;
 import org.example.ticTacToe.model.Player;
 
+import java.util.HashMap;
+
 public class ColumnWinningStrategy implements GameWinningStrategy{
+    private static HashMap<Integer, HashMap<Player,Integer>> colInfo = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Player lastplayerPlayed, Cell lastMoveCell) {
+        int col = lastMoveCell.getCol();
+        int dimension = board.getBoard().size();
+        if(colInfo.containsKey(col)){
+            HashMap<Player,Integer> map = colInfo.get(col);
+            if(map.containsKey(lastplayerPlayed)){
+                map.put(lastplayerPlayed,map.get(lastplayerPlayed)+1);
+            }else {
+                map.put(lastplayerPlayed,1);
+            }
+            colInfo.put(col,map);
+        }else{
+            HashMap<Player,Integer> map = new HashMap<>();
+            map.put(lastplayerPlayed,1);
+            colInfo.put(col,map);
+        }
+        if(colInfo.get(col).get(lastplayerPlayed)==dimension){
+            return true;
+        }
         return false;
     }
 }
--- src/main/java/org/example/ticTacToe/strategy/GameWinningStrategy/RowWinningStrategy.java
+++ src/main/java/org/example/ticTacToe/strategy/GameWinningStrategy/RowWinningStrategy.java
@@ -4,9 +4,30 @@ import org.example.ticTacToe.model.Board;
 import org.example.ticTacToe.model.Cell;
 import org.example.ticTacToe.model.Player;
 
+import java.util.HashMap;
+
 public class RowWinningStrategy implements GameWinningStrategy{
+    private static HashMap<Integer, HashMap<Player,Integer>> rowInfo = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Player lastplayerPlayed, Cell lastMoveCell) {
+        int row = lastMoveCell.getRow();
+        int dimension = board.getBoard().size();
+        if(rowInfo.containsKey(row)){
+            HashMap<Player,Integer> map = rowInfo.get(row);
+            if(map.containsKey(lastplayerPlayed)){
+                map.put(lastplayerPlayed,map.get(lastplayerPlayed)+1);
+            }else {
+                map.put(lastplayerPlayed,1);
+            }
+            rowInfo.put(row,map);
+        }else{
+            HashMap<Player,Integer> map = new HashMap<>();
+            map.put(lastplayerPlayed,1);
+            rowInfo.put(row,map);
+        }
+        if(rowInfo.get(row).get(lastplayerPlayed)==dimension){
+            return true;
+        }
         return false;
     }
 }
